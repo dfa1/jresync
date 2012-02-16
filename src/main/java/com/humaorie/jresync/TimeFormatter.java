5cf491e4d347687b779346d084e789cd3254aace
Davide Angelocola <davide.angelocola@gmail.com>
2012-02-16 21:57:19 +0100

    ref: removed comments

--- src/main/java/com/humaorie/jresync/TimeFormatter.java
+++ src/main/java/com/humaorie/jresync/TimeFormatter.java
@@ -1,15 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.humaorie.jresync;
 
-/**
- *
- * @author dfa
- */
 public interface TimeFormatter {
 
     String format(long time);
-    
 }
--- src/main/java/com/humaorie/jresync/TimeParser.java
+++ src/main/java/com/humaorie/jresync/TimeParser.java
@@ -1,15 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.humaorie.jresync;
 
-/**
- *
- * @author dfa
- */
 public interface TimeParser {
 
     long parse(String string);
-    
 }
